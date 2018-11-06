498d1ff523a1e15b8f040f47d6145c556869bd8d
Enigma <monstawoodwow@gmail.com>
2018-11-06 21:12:39 +0000

    New changes

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/// <reference path="../src/api.ts" />
+/// <reference path="../src/diablo3.ts" />
+/// <reference path="../src/wow.ts" />
+/// <reference path="../src/exceptions.ts" />
+/// <reference path="../src/httpRequest.ts" />
